979fde54d98cb6d9153f0019fb326a6952389f76
Aerol <adz10317@gmail.com>
2025-12-23 14:08:27 +0800

    Configure for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: This must match your repository name exactly for GitHub Pages to work
-  base: '/MSL-Philippines-Website-Sample/',
 })
